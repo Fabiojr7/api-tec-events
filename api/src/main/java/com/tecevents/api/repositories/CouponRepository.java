ddf28d239d6c13686cf78fdc81cd0c0c76f92210
Fabiojr7 <fabiojrsilveira09@gmail.com>
2024-06-17 13:50:21 -0300

    feat: add repositories.
    Reviewed-by: Fabiojr7 <fabiojrsilveira09@gmail.com>
    Refs #3

---
+++ api/src/main/java/com/tecevents/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.tecevents.api.repositories;
+
+import com.tecevents.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
---
+++ api/src/main/java/com/tecevents/api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.tecevents.api.repositories;
+
+import com.tecevents.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
