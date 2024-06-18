f274a9a4f77daf89124ef343c72c8cfa195a1090
Fabiojr7 <fabiojrsilveira09@gmail.com>
2024-06-18 17:23:59 -0300

    fix: refactoring code.
    Reviewed-by: Fabiojr7 <fabiojrsilveira09@gmail.com>
    Refs #12

--- api/src/main/java/com/tecevents/api/service/EventService.java
+++ api/src/main/java/com/tecevents/api/service/EventService.java
@@ -11,16 +11,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -60,17 +55,7 @@ public class EventService {
     public List<EventResponseDTO> getUpcomingEvents(int page, int size){
         Pageable pageable = PageRequest.of(page, size);
         Page<Event> eventsPage = this.eventRepository.findUpcomingEvents(new Date(), pageable);
-        return eventsPage.map(event -> new EventResponseDTO(
-                event.getId(),
-                event.getTitle(),
-                event.getDescription(),
-                event.getDate(),
-                event.getAddress() != null ? event.getAddress().getCity() : "",
-                event.getAddress() != null ? event.getAddress().getUf() : "",
-                event.getRemote(),
-                event.getEventUrl(),
-                event.getImgUrl()))
-                .stream().toList();
+        return mapEventsPage(eventsPage);
     }
 
     public List<EventResponseDTO> getFilteredEvents(int page, int size, String title, String city, String uf, Date startDate, Date endDate) {
@@ -86,17 +71,7 @@ public class EventService {
         Pageable pageable = PageRequest.of(page, size);
 
         Page<Event> eventsPage = this.eventRepository.findFilteredEvents(title, city, uf, startDate, endDate, pageable);
-        return eventsPage.map(event -> new EventResponseDTO(
-                        event.getId(),
-                        event.getTitle(),
-                        event.getDescription(),
-                        event.getDate(),
-                        event.getAddress() != null ? event.getAddress().getCity() : "",
-                        event.getAddress() != null ? event.getAddress().getUf() : "",
-                        event.getRemote(),
-                        event.getEventUrl(),
-                        event.getImgUrl()))
-                        .stream().toList();
+        return mapEventsPage(eventsPage);
     }
 
     public EventDetailsDTO getEventDetails(UUID eventId) {
@@ -121,5 +96,19 @@ public class EventService {
                 event.getImgUrl(),
                 event.getEventUrl(),
                 couponDTOs);
+    }
+
+    public List<EventResponseDTO> mapEventsPage(Page<Event> eventsPage){
+        return eventsPage.map(event -> new EventResponseDTO(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getDate(),
+                        event.getAddress() != null ? event.getAddress().getCity() : "",
+                        event.getAddress() != null ? event.getAddress().getUf() : "",
+                        event.getRemote(),
+                        event.getEventUrl(),
+                        event.getImgUrl()))
+                .stream().toList();
     }
 }
